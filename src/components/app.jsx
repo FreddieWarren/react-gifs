185f8c1621009a505ab4d4b2e168a2c9d7291d33
freddiewarren <fredswow@live.co.uk>
2020-05-25 16:00:31 +0100

    added App and search_bar components

---
+++ src/components/app.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import SearchBar from './search_bar.jsx'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar />
+        </div>
+        <div className="right-scene"></div>
+      </div>
+      );
+  }
+}
+
+export default App;
+
+
+// 52:43
--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
-
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+import App from './components/app.jsx';
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(
+    <App />,
+    root);
 }
+
+
