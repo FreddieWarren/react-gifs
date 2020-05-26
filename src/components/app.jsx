5f9e8234e1d97bd68923526cc57528d5e984f136
freddiewarren <fredswow@live.co.uk>
2020-05-26 22:30:32 +0100

    searchbar and APP updated

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,21 +1,57 @@
 import React, { Component } from 'react';
+import giphy from 'giphy-api';
 
-import SearchBar from './search_bar.jsx'
+import SearchBar from './search_bar';
+import Gif from './gif';
+import GifList from './gif_list';
+
+const GIPHY_API_KEY = '1KMPHCBIOe3hOjJwCJQX49sRc6cM0oIm';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "xT9IgDEI1iZyb2wqo8"
+    };
+  }
+
+  search = (query) => {
+    giphy({ apiKey: GIPHY_API_KEY, https: true })
+      .search({
+        q: query,
+        rating: 'g',
+        limit: 10
+      }, (err, result) => {
+        this.setState({
+          gifs: result.data
+        });
+      });
+  }
+
+  selectGif = (id) => {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
   render() {
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchFunction={this.search} />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
+          </div>
         </div>
-        <div className="right-scene"></div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
+        </div>
       </div>
-      );
+    );
   }
 }
 
 export default App;
 
-
-// 52:43
--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  handleUpdate = (event) => {
+    this.props.searchFunction(event.target.value);
+  }
+
   render() {
     return (
-      <input type="text" className="form-control form-search"/>
-      );
+      <input
+        type="text"
+        className="form-control form-search"
+        onChange={this.handleUpdate}
+      />
+    );
   }
 }
 
